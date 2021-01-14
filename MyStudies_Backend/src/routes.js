e6490fc296ad789b72546da83a470538a299b877
Kayk Mascarenhas <ksm.kayk@outlook.com>
2021-01-14 17:45:57 -0300

    adicionado store update e delete para teachercontroller

--- MyStudies_Backend/src/routes.js
+++ MyStudies_Backend/src/routes.js
@@ -4,5 +4,8 @@ const teacherController = require("./controllers/teacherController")
 const routes = Router();
 
 routes.get("/teacher", teacherController.index);
+routes.post("/teacher", teacherController.store);
+routes.put("/teacher/:id", teacherController.update);
+routes.delete("/teacher/:id", teacherController.delete);
 
 module.exports = routes;
